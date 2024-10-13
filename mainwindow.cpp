b22e7100ac4e5e3e6ca65099bb3456d3c684bd2a
Breno Prestes <bcaua205@gmail.com>
2024-10-12 22:15:36 -0300

    Update mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QtGui>
+#include "points.h"
+#include "primitiveobject.h"
+#include "factoryobject.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,8 +21,22 @@ void MainWindow::paintEvent(QPaintEvent* event) {
     QPainter painter(this);
 
     auto size = this->size();
-    auto offset = 4;
+    auto offset = 500;
 
+    vector<PrimitiveObject> list = FactoryObject().create_list();
 
-    painter.drawEllipse(0, 0, size.width() - offset, size.height() - offset);
+    auto point = list[0];
+    auto line = list[1];
+    auto triangle = list[2];
+
+    QPolygon polygon;
+    polygon << QPoint(triangle.points[0].get_firstPoint(), triangle.points[0].get_secondPoint())
+            << QPoint(triangle.points[1].get_firstPoint(), triangle.points[1].get_secondPoint())
+            << QPoint(triangle.points[2].get_firstPoint(), triangle.points[2].get_secondPoint());
+
+    painter.drawPoint(point.points[0].get_firstPoint(), point.points[0].get_secondPoint());
+    painter.drawLine(line.points[0].get_firstPoint(), line.points[0].get_secondPoint(), size.width() - offset, size.height() - offset);
+    painter.drawPolygon(polygon);
 }
+
+
